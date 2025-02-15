9e26ee8c7490927af54989b8c3772b7db1408233
David Soria Parra <experimentalworks@gmail.com>
2025-02-15 23:10:33 +0000

    init

---
+++ devenv.nix
@@ -0,0 +1,9 @@
+{ pkgs, lib, config, ... }: {
+  languages.java = {
+    enable = true;
+    jdk.package = pkgs.jdk;
+    maven.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}

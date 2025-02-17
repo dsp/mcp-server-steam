75a9df519c544089c2ac5a512cd4fa661f3fb84d
David Soria Parra <experimentalworks@gmail.com>
2025-02-17 13:12:40 +0000

    fix: devenv

--- devenv.nix
+++ devenv.nix
@@ -6,13 +6,11 @@
     maven.enable = true;
   };
 
-  # watch local changes and build the project to ./dist
-  processes.build.exec = "${pkgs.watchexec}/bin/watchexec mvn package";
-  processes.serve.exec = "java -jar target/mcp-steam-1.0-SNAPSHOT.jar";
-  containers."prod".name = "mcp-steam";
-  containers."prod".startupCommand = config.processes.serve.exec;
+  enterTest = ''
+    mvn compile
+    mvn spotless:check
+  '';
 
-  # containers."prod".copyToRoot = ./target;
-  # containers."prod".startupCommand = "java -jar /mcp-steam-1.0-SNAPSHOT.jar";
-  # See full reference at https://devenv.sh/reference/options/
+  # watch local changes and build the project to ./dist
+  # processes.build.exec = "${pkgs.watchexec}/bin/watchexec mvn package";
 }
